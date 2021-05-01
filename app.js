e6f21dbe11ea24ad99103c63b4062b77a741147d
Srijan67 <82119082+Srijan67@users.noreply.github.com>
2021-05-01 15:37:43 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const personalSetting = document.querySelector('.accicon');
+const menuBtn = document.querySelector('.menu-btn');
+const title = document.querySelector('#title');
 personalSetting.addEventListener('click', function(e) {
     alert('HELLO!');
 });
+// menuBtn.addEventListener('click', function(e) {
+//     title.style.display = 'none';
+//     menuBtn.addEventListener('click', function(e) {
+//         title.style.display= 'block';
+//     });
+// });
+const navLinks = document.querySelector('.nav-links');
